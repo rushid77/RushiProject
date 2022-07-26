0bdacf079bb7fdc1f4540ad224a299524c9e7207
Rushikesh <Rushikesh@LAPTOP-BIQOUOB6>
2022-07-26 14:11:32 +0530

    Hi 4th commit

--- RushiProject/src/com/github/Sample1.java
+++ RushiProject/src/com/github/Sample1.java
@@ -7,7 +7,8 @@ public static void main(String[] args)
 	System.out.println("Hello java world");
 	int a= 20;
 	int b= 30;
-	int sum=a+b;
-	System.out.println(sum);
+	int c= 40;
+	int multi=a*b*c;
+	System.out.println(multi);
 }
 }
---
+++ RushiProject/src/com/github/Sample4.java
@@ -0,0 +1,9 @@
+package com.github;
+
+public class Sample4 
+{
+	public static void main(String[] args) {
+		
+	}
+
+}
